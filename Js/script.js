e648df32af7afbc92143f50667f938ad0e96ac42
alessandra martera <ale.martera@gmail.com>
2023-05-02 16:07:20 +0200

    add class color

--- Js/script.js
+++ Js/script.js
@@ -1,22 +1,39 @@
 
-// scrivi un programma che stampi i numeri da 1 a 100;
-for ( let i =1; i<= 100; i++) {
-// con i multipli di 3 e 5 stampare "FizzBuzz"
+
+// Write a program that stamp all number from 1 to 100;
+for ( let i =1; i<= 100; i++) {    
+let num;
+
+const squareContainer = document.getElementById("container");
+let square = document.createElement('div');
+square.classList.add("square");
+squareContainer.append(square);
+
+
+
+    // IF multiple of 3 and 5 stamp "FizzBuzz"
     if ( i%3 === 0 && i%5 === 0 ){
-        console.log("FizzBuzz");
-    }
-    // con i multipli di 3 stampare "Fizz"
-    else if ( i%3 === 0) {
-        console.log("Fizz");
-    }
-    // con i multipli di 5 stampare "Buzz"
-    else if( i%5 === 0) {
-        console.log("Buzz");
-    }
-    else {
-        console.log(i)
+        num = "FizzBuzz";
+        square.classList.add("fizzbuzz");
     }
 
+    // Else multiple of 3 stamp "Fizz"
+    else if ( i%3 === 0) {
+        num = "Fizz";
+        square.classList.add("fizz");
+    }
+
+    // Else multiple of 3 stamp "Buzz"
+    else if( i%5 === 0) {
+       num = "Buzz";
+       square.classList.add("buzz");
+    }
+    else {
+        num = i;
+    }
+
+    square.append(num);
+    console.log(num);
 }
 
 
